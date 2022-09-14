81d013489211758a64b9adc97203a59ed14cc5d8
Glitch (getting-started-discord) <none>
2022-09-14 17:10:48 +0000

    🚇🐌 Checkpoint ./app.js:17622919/217:48712987/344

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ import {
   TEST_COMMAND,
   HasGuildCommands,
 } from "./commands.js";
+//import react from "convex";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+console.log(ConvexReactClient);
 
 // Create an express app
 const app = express();
@@ -27,6 +30,8 @@ app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
 // Store for in-progress games. In production, you'd want to use a DB
 const activeGames = {};
+
+const url = "aware-spoonbill-23";
 
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
